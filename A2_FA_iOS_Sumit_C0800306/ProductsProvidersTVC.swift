d4611146700e3f3bd2732d91f037122dc9a49f7a
Sumit Desai <sumitdesai1997@gmail.com>
2021-05-22 12:28:05 -0700

    deleting data from the core data

--- A2_FA_iOS_Sumit_C0800306/ProductsProvidersTVC.swift
+++ A2_FA_iOS_Sumit_C0800306/ProductsProvidersTVC.swift
@@ -57,17 +57,16 @@ class ProductsProvidersTVC: UITableViewController {
     }
     */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            deleteProduct(product: productList[indexPath.row])
+            // after deleting the data from the core data it is mandatory to save the core data
+            saveProducts()
+            productList.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
     /*
     // Override to support rearranging the table view.
@@ -123,6 +122,11 @@ class ProductsProvidersTVC: UITableViewController {
         } catch{
             print("error: \(error.localizedDescription)")
         }
+    }
+    
+    // deleting data from the context
+    func deleteProduct(product: Product){
+        context.delete(product)
     }
     
 }
