6d6f60ac44171afa7f36078bb0bbdfa0b4f9d446
Sumit Desai <sumitdesai1997@gmail.com>
2021-05-22 00:27:41 -0700

    Adding new view controller and ProductVC

---
+++ A2_FA_iOS_Sumit_C0800306/ProductVC.swift
@@ -0,0 +1,29 @@
+//
+//  ProductVC.swift
+//  A2_FA_iOS_Sumit_C0800306
+//
+//  Created by Sumit Desai on 22/05/21.
+//
+
+import UIKit
+
+class ProductVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
