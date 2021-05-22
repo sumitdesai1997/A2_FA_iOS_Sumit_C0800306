7b41c736ff79ef59d1a50b3055a387b29a660a41
Sumit Desai <sumitdesai1997@gmail.com>
2021-05-21 21:31:47 -0700

    Initial Commit

---
+++ A2_FA_iOS_Sumit_C0800306Tests/A2_FA_iOS_Sumit_C0800306Tests.swift
@@ -0,0 +1,33 @@
+//
+//  A2_FA_iOS_Sumit_C0800306Tests.swift
+//  A2_FA_iOS_Sumit_C0800306Tests
+//
+//  Created by Sumit Desai on 21/05/21.
+//
+
+import XCTest
+@testable import A2_FA_iOS_Sumit_C0800306
+
+class A2_FA_iOS_Sumit_C0800306Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
