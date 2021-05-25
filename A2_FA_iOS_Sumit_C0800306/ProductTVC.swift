dc96b80abdf5b18bbd03ce7c956234ae8d27c6f1
Sumit Desai <sumitdesai1997@gmail.com>
2021-05-25 16:30:46 -0700

    Adding new table view controller screen and ProductTVC

---
+++ A2_FA_iOS_Sumit_C0800306/ProductTVC.swift
@@ -0,0 +1,29 @@
+//
+//  ProductTVC.swift
+//  A2_FA_iOS_Sumit_C0800306
+//
+//  Created by Sumit Desai on 25/05/21.
+//
+
+import UIKit
+
+class ProductTVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
